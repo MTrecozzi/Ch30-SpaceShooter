45b5361c33288d4ec41ac0eddcab2992978134bc
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-09-22 13:26:52 -0400

    Prefabs for Enemy Ships

---
+++ Assets/__Scripts/BoundsCheck.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+
+    public float radius = 1f;
+
+    public float camWidth;
+    public float camHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camHeight = Camera.main.orthographicSize;                           
+
+        camWidth = camHeight * Camera.main.aspect;
+    } 
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+
+        if (pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+        }
+
+        if (pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+
+        if (pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+
+        transform.position = pos;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+}
--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+
+    static public Hero s;
+
+    [Header("Set In Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    private void Awake()
+    {
+        if (s == null)
+        {
+            s = this;
+        } else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero Singleton!");
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +35,16 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 newPos = transform.position;
+
+        newPos.x += xAxis * speed * Time.deltaTime;
+        newPos.y += yAxis * speed * Time.deltaTime;
+
+        transform.position = newPos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
     }
 }
