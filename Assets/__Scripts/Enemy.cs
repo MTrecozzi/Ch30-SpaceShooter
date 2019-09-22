e83a2d3df415dc097e3f5108fe06560b0ab00b82
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-09-22 17:59:36 -0400

    Added Damage Blink

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -12,6 +12,8 @@ public class Enemy : MonoBehaviour
     public float health = 10;
     public int score = 100;
 
+    public float showDamageDuration = 0.1f;
+
     public Vector3 pos
     {
         get { return this.transform.position; }
@@ -22,9 +24,29 @@ public class Enemy : MonoBehaviour
 
     protected BoundsCheck bndCheck;
 
+    [Header("Set Dynamically: Enemy")]
+    public Color[] originalColors;
+
+    public Material[] materials;
+
+    public bool showingDamage = false;
+
+    public float damageDoneTime;
+
+    public bool notifiedOfDestruction = false; // will be used later
+
     private void Awake()
     {
         bndCheck = GetComponent<BoundsCheck>();
+
+        materials = Utils.GetAllMaterials(gameObject);
+
+        originalColors = new Color[materials.Length];
+
+        for (int i = 0; i < materials.Length; i++)
+        {
+            originalColors[i] = materials[i].color;
+        }
     }
 
     // Start is called before the first frame update
@@ -50,6 +72,8 @@ public class Enemy : MonoBehaviour
 
                 health -= Main.GetWeaponDefinition(p.type).damageOnHit;
 
+                ShowDamage();
+
                 if (health <= 0)
                 {
                     Destroy(this.gameObject);
@@ -65,16 +89,46 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    void ShowDamage()
+    {
+        foreach (Material m in materials)
+        {
+
+            m.color = Color.red;
+
+        }
+
+        showingDamage = true;
+
+        damageDoneTime = Time.time + showDamageDuration;
+    }
+
+    void UnShowDamage()
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+
+            materials[i].color = originalColors[i];
+
+        }
+
+        showingDamage = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
         Move();
 
+        if (showingDamage && Time.time > damageDoneTime)
+        {
+            UnShowDamage();
+        }
+
+
         if (bndCheck != null && bndCheck.offDown)
         {
-            
                 Destroy(gameObject);
-            
         }
     }
 
--- Assets/__Scripts/Utils.cs
+++ Assets/__Scripts/Utils.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
+
+
 
 public class Utils : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //============================ Materials Functions ===========================\\
+    // Returns a list of all Materials on this GameObject and its children
+
+    static public Material[] GetAllMaterials(GameObject go)
+    {              
+
+        Renderer[] rends = go.GetComponentsInChildren<Renderer>();  
+
+
+
+        List<Material> mats = new List<Material>();
+
+        foreach (Renderer rend in rends)
+        {   
+
+            mats.Add(rend.material);
+        }
+
+
+
+        return (mats.ToArray());                                            
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
