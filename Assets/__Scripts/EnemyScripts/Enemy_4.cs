acaacd3ea4b3aa4237dfcd1f2bf9494fca8ddfc4
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-09-22 20:36:24 -0400

    Enemy 4 MovementPattern

--- Assets/__Scripts/EnemyScripts/Enemy_4.cs
+++ Assets/__Scripts/EnemyScripts/Enemy_4.cs
@@ -6,18 +6,50 @@ public class Enemy_4 : Enemy
 {
 
     private Vector3 p0, p1; // two points to interpolate
+    private float timeStart;
+
+    private float duration = 4;
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        p0 = p1 = pos;
+
+        InitMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitMovement()
     {
-        
+        p0 = p1;
+
+        // assign a new on-screen location to p1;
+
+        float widMinRad = bndCheck.camWidth - bndCheck.radius;
+        float hgtMinRad = bndCheck.camHeight - bndCheck.radius;
+
+        p1.x = Random.Range(-widMinRad, widMinRad);
+        p1.y = Random.Range(-hgtMinRad, hgtMinRad);
+
+        timeStart = Time.time;
+
+    }
+
+    public override void Move()
+    {
+        float u = (Time.time - timeStart) / duration;
+
+        if (u >= 1)
+        {
+            InitMovement();
+
+            u = 0;
+        }
+
+        u = 1 - Mathf.Pow(1 - u, 2);
+        pos = (1 - u) * p0 + u * p1;
     }
 }
